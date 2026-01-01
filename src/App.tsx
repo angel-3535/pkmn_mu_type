8b3f24c3f931d0854ebdbf9abf942771ed674238
Angel Pena <theangel3535@gmail.com>
2025-12-31 19:59:48 -0500

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { PokemonSprite } from "./components/Sprite";
-import { AnimatePresence, motion } from "motion/react";
+import { motion } from "motion/react";
 import { useQuery } from "@tanstack/react-query";
 import { getRandomType, type PokemonType } from "./utils/types";
 import { getEffectiveness } from "./utils/typeDmgCalc";
